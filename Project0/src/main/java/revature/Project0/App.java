ec91a9583faa5cffee6ebe83ad1cbe43a4e0df7a
gk <georgepulikkayath@gmail.com>
2019-09-05 13:22:58 -0400

    methods changed

--- Project0/src/main/java/revature/Project0/App.java
+++ Project0/src/main/java/revature/Project0/App.java
@@ -13,38 +13,40 @@ public class App
         System.out.println("First Project\n");
         System.out.println("-----------------");
         Scanner myObj = new Scanner(System.in);
-        String userName;
-        System.out.println("Enter username\n"); 
-        userName = myObj.nextLine();   
-           
-        System.out.println("Username is: " + userName);  
-        System.out.println("Enter the password\n");
-        String pswd;
-        pswd=myObj.nextLine();
-        System.out.println("The password is"+pswd);
-        do {
-            System.out.println("\n\n       Menu");
-            System.out.println("--------------------------------------");
-            System.out.println("1-View The Cars");
-            System.out.println("2-Make offer for car");
-            System.out.println("3-View the cars that I own");
-            System.out.print("\nSelect a Menu Option: ");
-        try {
-            int input = myObj.nextInt(); // Get user input from the keyboard 
-
-
-            switch (input) {
-            case 1:  System.out.println("You have choosen 1");
-                     break;
-            case 2:  // do something
-                     break;
-          
-           }
-          } catch (NumberFormatException e) {  }
-
+   
+        System.out.println("Do you have an account?if yes press y otherwise press N");
+        char c=myObj.next().charAt(0);
+        if((c=='y')||(c=='Y')) {
+        	System.out.println("it matches");
+        	Login();
         }
-        while(true); // Display the menu until the user closes the program
+       
       
+       
+        else if((c=='n')||(c=='N')) {
+        	System.out.println("Create an account");
+        }
         
+       
     }
+    public static void Login() {
+		// TODO Auto-generated method stub
+    	String UserName;
+    	String Runame="admin";
+    	  Scanner myObj = new Scanner(System.in);
+		System.out.println("Enter username");
+		UserName=myObj.nextLine();
+		System.out.println("The username is:"+UserName);
+		System.out.println("Enter the password");
+		String pswd=myObj.nextLine();
+		System.out.println("The password is"+pswd);
+		
+		if(UserName.equals(Runame)) {
+			System.out.println("Go to admin menue");
+		}
+		
+	}
+
+	
 }
+
