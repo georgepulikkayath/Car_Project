073388badeb3581f149a38b54877d530a2bb3dab
gk <georgepulikkayath@gmail.com>
2019-09-05 22:41:38 -0400

    files added

--- Project0/src/main/java/revature/Project0/App.java
+++ Project0/src/main/java/revature/Project0/App.java
@@ -10,44 +10,76 @@ public class App
 {
     public static void main( String[] args )
     {
+    	while(true) {
         System.out.println("First Project\n");
         System.out.println("-----------------");
         Scanner myObj = new Scanner(System.in);
    
         System.out.println("Do you have an account?if yes press y otherwise press N");
-        char c=myObj.next().charAt(0);
-        if((c=='y')||(c=='Y')) {
-        	System.out.println("it matches");
-        	Login();
-        }
-       
-      
-       
-        else if((c=='n')||(c=='N')) {
-        	System.out.println("Create an account");
-        	createAccount();
-        }
         
+        char c=myObj.next().charAt(0);
+        char check=Character.toLowerCase(c);
+        switch(check) {
+        case 'y':Login();
+       
+        break;
+        case 'n':createAccount();
+        break;
+        default:System.out.println("Invalid choice");
+        break;
+    
+        }
+    	}
        
     }
-    public static void createAccount() {
+  
+	public static void createAccount() {
 		// TODO Auto-generated method stub
+    	
     	Scanner myobj=new Scanner(System.in);
-    	System.out.println("Create an account");
-    	System.out.println("Enter your email id");
+    	
+    	System.out.println("Registeration Page");
+    	System.out.println("Enter your email id:");
     	String emailId=myobj.nextLine();
-    	System.out.println("Enter your username");
+    	System.out.println("Enter your username:");
     	String uname=myobj.nextLine();
-    	System.out.println("Enter a password");
+    	System.out.println("Enter a password:");
     	String pswd=myobj.nextLine();
+      Check(emailId,uname,pswd);
+    	  
+    
+    	
 		
+	}
+	
+	
+
+	public static  void Check(String emailId,String uname,String pswd) {
+	  	String check="";
+  	  String regex = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
+		if(emailId.equals(check)||(uname.equals(check))||(pswd.equals(check))){
+    		System.out.println("fill the fields");
+    		createAccount();
+    	}
+    	
+    	else if(emailId.matches(regex)==false) {
+    		System.out.println("email is not valid");
+    		createAccount();
+    	}
+    	else {
+    		System.out.println("It is  a valid entry");
+    		Login();
+    	}
+    	
 	}
 	public static void Login() {
 		// TODO Auto-generated method stub
     	String UserName;
     	String Runame="admin";
     	  Scanner myObj = new Scanner(System.in);
+    	  System.out.println("Login page");
 		System.out.println("Enter username");
+		
 		UserName=myObj.nextLine();
 		System.out.println("The username is:"+UserName);
 		System.out.println("Enter the password");
